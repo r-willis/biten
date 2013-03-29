337f54a56dda9e0487f34b39b6452d05ad4971f5
Randy J. Willis <willis.randy@ymail.com>
2013-03-29 17:08:35 +0000

    Base58 encoding

--- apps/biten/src/util.erl
+++ apps/biten/src/util.erl
@@ -5,7 +5,7 @@
 %%% --------------------------------------------------------------------------
 
 -module(util).
--export([hexdump/1, ip_to_str/1, crc_to_str/1, senddump/1, startdump/0, dump/0, strftime/1]).
+-export([hexdump/1, hex_to_bin/1, to_base58/1, base58_enc/2, ip_to_str/1, crc_to_str/1, senddump/1, startdump/0, dump/0, strftime/1]).
 
 hexdump(B) -> hexdump(B, 0).
 
@@ -51,3 +51,61 @@ dump() ->
 
 strftime({{Y, M, D}, {H, Min, Sec}}) ->
     io_lib:format("~4..0b.~2..0b.~2..0b ~2..0b:~2..0b:~2..0b", [Y, M, D, H, Min, Sec]).
+
+leading_zeros(B) ->
+    leading_zeros(B, 0).
+
+leading_zeros(<<0, R/bytes>>, X) ->
+    leading_zeros(R, X + 1);
+
+leading_zeros(R, X) when is_binary(R) ->
+    X.
+
+-define(CODE_ARRAY, array:from_list("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")).
+
+code(X) when X < 58 ->
+    array:get(X, ?CODE_ARRAY).
+
+to_base58(B) ->
+    N = size(B)*8,
+    <<X:N/big>> = B,
+    Z = leading_zeros(B),
+    prepend(to_base58(X, []), $1, Z).
+
+to_base58(0, L)  ->
+    L;
+
+to_base58(N, L) ->
+    to_base58(N div 58, [code(N rem 58)|L]).
+
+prepend(L, S, N) when N > 0 ->
+    prepend([S|L], S, N-1);
+
+prepend(L, _, 0) ->
+    L.
+
+base58_enc(Ver, B) ->
+    S1 = <<Ver, B/bytes>>,
+    S2 = b_crypto:crc(S1),
+    S3 = <<S1/bytes, S2/bytes>>,
+    to_base58(S3).
+
+digit(X) when X >= $0, X =< $9 ->
+    X - $0;
+
+digit(X) when X >= $a, X =< $z ->
+    X - $a + 10;
+
+digit(X) when X >= $A, X =< $Z ->
+    X - $A + 10.
+
+hex_to_bin(S) ->
+    binary:list_to_bin(hex_to_bin(S, [])).
+
+hex_to_bin([], R) ->
+    lists:reverse(R); 
+hex_to_bin([$\  | T], R) ->
+    hex_to_bin(T, R);
+
+hex_to_bin([A, B | T], R) ->
+    hex_to_bin(T, [digit(A)*16+digit(B)|R]).
